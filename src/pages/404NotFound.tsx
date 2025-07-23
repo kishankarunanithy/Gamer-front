5818d830d5f96bab4e46778ff63484a2af0b2444
jeyakishandev <k.jeyakishan@gmail.com>
2025-07-23 10:28:05 +0200

    remettre images note found

--- src/pages/404NotFound.tsx
+++ src/pages/404NotFound.tsx
@@ -1,13 +1,13 @@
 import "../App.css"
 import { Link } from "react-router-dom"
-
+import  ErrorFound  from "../../public/img/dino-4575130_640.png";
 export default function NotFound() {
 
     return (
         <>
         <div className="main-404">
             <p className="default-text main-sentence-404">Ce n'est pas par ici que tu trouveras de nouveaux challenges à réaliser !</p>
-            <img className="dino-car-404" src="../../public/img/dino-4575130_640.png"/>
+            <img className="dino-car-404" src={ErrorFound}/>
             <p className="default-text home-link-404"> <Link to={`/`} > Clique ici pour retourner à l'accueil ! </Link> </p>
         </div>
         </>
